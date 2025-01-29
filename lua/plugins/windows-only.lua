a0447ba66e507b9b9f55cefa978a02be7d52541c
nomnivore <6979410+nomnivore@users.noreply.github.com>
2025-01-29 02:04:03 -0500

    add some neovide configs

--- lua/plugins/windows-only.lua
+++ lua/plugins/windows-only.lua
@@ -4,6 +4,12 @@ local function is_windows()
   return jit.os:find("Windows")
 end
 
+if vim.g.neovide then
+  vim.o.shell = "pwsh.exe -nologo"
+  vim.o.guifont = "Monaspace Neon,Hack Nerd Font:h14"
+  vim.g.neovide_title_background_color = "#181825"
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
